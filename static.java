fc7e2f9bf63643b3e58d6992b4fb316b2527bbf0
Abhinav <gabhinav.g28@gmail.com>
2025-08-02 12:44:44 +0530

    Add files via upload

---
+++ static.java
@@ -0,0 +1,12 @@
+package Java;
+
+public class static1 {
+		  public static void main(String[] args)
+		  {
+		  byte b = 10;
+		  int i;
+		  i = b;
+		  System.out.println(b);
+		  System.out.println(i);
+		  }
+		}
