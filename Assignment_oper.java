11ba7b37f3dfb73e36fc845121985f4e32b47f49
Abhinav <gabhinav.g28@gmail.com>
2025-08-05 14:02:17 +0530

    Add files via upload

---
+++ Assignment_oper.java
@@ -0,0 +1,16 @@
+package Scope;
+
+public class Assignment_oper {
+    public static void main(String[] args)
+  {
+    int a=4,b=1;
+	System.out.println(a);//4
+	System.out.println(b);//1
+	a = a * b;
+   	b = a / b;
+	a = a / b;
+	System.out.println(a);//1
+	System.out.println(b);//4
+}
+
+}
