1717d5ea8ceef01b3513ba1e05ad53263ea9c594
Abhinav <gabhinav.g28@gmail.com>
2025-08-05 14:00:43 +0530

    Add files via upload

---
+++ While.java
@@ -0,0 +1,15 @@
+package Scope;
+
+public class While {
+	public static void main(String[] args) 
+	 {
+	 int fact = 1;
+	 int x = 5;
+	 while(x>=1) 
+	  {
+	 fact = fact * x; 
+	 x--;
+	 }
+	 System.out.println("Factorial of the given number is:: "+fact);
+	} 
+}
